4eaf48ceaada430e0f54c26f8ffb1123f1e366e9
CapoMK25 <parisburner6969@gmail.com>
2026-01-23 13:02:22 +0200

    Added a working S3 bucket

---
+++ iac/s3.py
@@ -0,0 +1,34 @@
+"""
+Troposphere template: S3 bucket for regional-map-2024 website
+Works with LocalStack
+"""
+
+from troposphere import Template, Ref, Output
+from troposphere.s3 import Bucket, WebsiteConfiguration
+
+# Create the template
+t = Template()
+t.set_description("LocalStack S3 bucket for regional-map-2024 hosting")
+
+# Bucket name
+bucket_name = "regional-map-2024-website"
+
+# Create the S3 bucket
+s3_bucket = t.add_resource(
+    Bucket(
+        "RegionalMap2024Bucket",
+        BucketName=bucket_name,
+        WebsiteConfiguration=WebsiteConfiguration(
+            IndexDocument="index.html"
+        )
+    )
+)
+
+# Output the bucket name
+t.add_output(Output(
+    "BucketName",
+    Description="S3 bucket for regional-map-2024",
+    Value=Ref(s3_bucket)
+))
+
+print(t.to_yaml())
