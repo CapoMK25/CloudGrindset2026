ef86edf2106b56926d8168eeb06ee51173aafff0
CapoMK25 <parisburner6969@gmail.com>
2026-02-09 13:25:34 +0200

    Updated the architecture workflow

--- iac/docs.py
+++ iac/docs.py
@@ -1,11 +1,17 @@
+"""Module to generate architecture diagrams."""
+# pylint: disable=expression-not-assigned, pointless-statement, missing-final-newline
+
 from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.compute import EC2
-from diagrams.aws.network import ALB, PublicSubnet
+from diagrams.aws.network import ALB
 from diagrams.aws.security import IAMRole
 from diagrams.aws.storage import S3
 
-with Diagram("CloudGrindset Architecture", show=False, filename="assets/architecture", direction="LR"):
-    
+with Diagram("CloudGrindset Architecture",
+            show=False,
+            filename="assets/architecture",
+            direction="LR"):
+
     with Cluster("AWS Cloud"):
         iam = IAMRole("Instance Profile")
         logs = S3("ALB Access Logs")
@@ -14,7 +20,7 @@ with Diagram("CloudGrindset Architecture", show=False, filename="assets/architec
             with Cluster("Public Subnets"):
                 lb = ALB("Web ALB")
                 server = EC2("Web Server")
-                
+
                 # Logical Flow
                 lb >> Edge(label="port 80") >> server
                 server - iam
