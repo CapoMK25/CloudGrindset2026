9d200bc91745ce5d27965461f6f50b0489aac820
CapoMK25 <parisburner6969@gmail.com>
2026-02-09 14:25:12 +0200

    fixed the failure, now testing again

--- iac/docs.py
+++ iac/docs.py
@@ -25,4 +25,3 @@ with Diagram("CloudGrindset Architecture",
                 lb >> Edge(label="port 80") >> server
                 server - iam
                 lb - logs
-unused_variable = "I will break the linter" # Pylint will flag this (W0612)
